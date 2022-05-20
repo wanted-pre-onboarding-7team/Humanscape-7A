a0d8e04b2c9d597c663769fb1b7a993aa9f422df
jiiiooiiij <88502596+HANJISUN@users.noreply.github.com>
2022-05-20 17:19:23 +0900

    chore: 사용하지 않는 코드 삭제

--- src/routes/ReactQuerySearch/index.tsx
+++ src/routes/ReactQuerySearch/index.tsx
@@ -1,28 +1,9 @@
-import { useQuery } from 'react-query'
-
 import SEO from 'components/Seo'
-import { getOpenDiseaseAPi } from 'services/disease'
+import useQuerySearch from 'hooks/useQuerySearch'
 import { SearchResult } from 'components/Search/SearchResult/ResultContainer'
-import { useRecoilValue } from 'recoil'
-import { searchState } from 'states/disease'
 
 const ReactQuery = () => {
-  const search = useRecoilValue(searchState)
-
-  const { data } = useQuery(
-    ['getDiseaseAPi', search],
-    () =>
-      getOpenDiseaseAPi({ searchText: search }).then((res) => {
-        return res.data.response.body.items
-      }),
-    {
-      // 쿼리가 없는 경우
-      enabled: !!search,
-      staleTime: 2 * 60 * 1000,
-      cacheTime: Infinity,
-      suspense: true,
-    }
-  )
+  const { data } = useQuerySearch()
 
   return (
     <div>
